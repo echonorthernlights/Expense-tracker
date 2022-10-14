d31ecc702a40137988737ff002bcff359f21fdfc
nlights <elhassanianas@gmail.com>
2022-10-14 16:18:41 +0100

    main changes

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -2,15 +2,16 @@ import React, { useContext } from 'react'
 import { GlobalContext, GlobalProvider } from '../context/GlobalState'
 
 export const Balance = () => {
-    
-    const {transactions} = useContext(GlobalContext)
+
+    const { transactions } = useContext(GlobalContext)
     const amounts = transactions.map((transaction) => transaction.amount)
     console.log(amounts)
 
     const total = amounts.reduce((acc, item) => acc = acc + item, 0).toFixed(2)
     return (
-        <div>
+        <div className=''>
             <h4>Your Balance</h4>
+
             <h1>${total}</h1>
         </div>
     )
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,20 +1,21 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 import { Transaction } from './Transaction'
+// comment 3
 
 export const TransactionsList = () => {
-  const {transactions} = useContext(GlobalContext)
-    return (
-        <div>
-             <h3>History</h3>
+  const { transactions } = useContext(GlobalContext)
+  return (
+    <div>
+      <h3>History</h3>
       <ul id="list" className="list">
         {transactions.map((transaction) => {
-          
+
           return (
-            <Transaction key={transaction.id} transaction={transaction}/>
+            <Transaction key={transaction.id} transaction={transaction} />
           )
         })}
       </ul>
-        </div>
-    )
+    </div>
+  )
 }
