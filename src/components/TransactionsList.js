bc67d81225e0bdb346299122a577874d6bef42d4
nlights <elhassanianas@gmail.com>
2022-10-14 16:19:53 +0100

    new_branch changes

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,20 +1,20 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 import { Transaction } from './Transaction'
-
+// conflict comment
 export const TransactionsList = () => {
-  const {transactions} = useContext(GlobalContext)
-    return (
-        <div>
-             <h3>History</h3>
+  const { transactions } = useContext(GlobalContext)
+  return (
+    <div>
+      <h3>History</h3>
       <ul id="list" className="list">
         {transactions.map((transaction) => {
-          
+
           return (
-            <Transaction key={transaction.id} transaction={transaction}/>
+            <Transaction key={transaction.id} transaction={transaction} />
           )
         })}
       </ul>
-        </div>
-    )
+    </div>
+  )
 }
